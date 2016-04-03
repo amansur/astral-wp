84ca3adaebed749c4a37391074f507704b589a9a
Arafat Mansur <amansur@gmail.com>
2016-04-03 18:46:15 -0400

    Literal change: Row -> Media

--- library/project-field-group.php
+++ library/project-field-group.php
@@ -81,7 +81,7 @@ acf_add_local_field_group(array (
 				'class' => '',
 				'id' => '',
 			),
-			'button_label' => 'Add Row',
+			'button_label' => 'Add Media',
 			'min' => '',
 			'max' => '',
 			'layouts' => array (
