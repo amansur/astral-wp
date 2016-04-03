80ce687af0a15898b80df3f0fb37afe08110e708
Arafat Mansur <amansur@gmail.com>
2016-04-03 19:04:18 -0400

    More angular test

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog" ng-app="listProject">
 								<div ng-repeat="project in projects">
-									{{project}}
+									{{project.title}}
 								</div>
 								{{5+7}}
 							
