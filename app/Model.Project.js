25fc82687b0355b047b3dc05f9ce2d0247344899
Arafat Mansur <amansur@gmail.com>
2016-06-26 23:13:09 -0400

    featured image - hide on mobile

--- app/Model.Project.js
+++ app/Model.Project.js
@@ -11,7 +11,7 @@ function Project(id, slug, name, description, tags, featureImageUrl, media) {
 
 	self.showFeatureImage = function () {
 		if (self.featureImage !== undefined && self.featureImage !== null && self.featureImage !== "") {
-			self.featureImageIsVisible(true);
+			self.featureImageIsVisible(window.innerWidth > 767); // true
 		}
 	}
 
