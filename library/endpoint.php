db7c5d0d074cbf90082a2e28c5495c421382d374
Arafat Mansur <amansur@gmail.com>
2016-09-18 12:58:59 -0400

    DESC -> ASC order by

--- library/endpoint.php
+++ library/endpoint.php
@@ -45,8 +45,8 @@ class AstralController extends WP_REST_Controller {
 		$postType = 'project'; //$postType = $this->post_type;
 		$args                         = array();
 		$args['order']                = 'ASC';
-		$args['orderby']              = 'name';
-		//$args['meta_key']			  = 'display_name';
+		$args['orderby']              = 'meta_value';
+		$args['meta_key']			  = 'display_name';
 		$args['posts_per_page']       = $request['per_page'] ? $request['per_page'] : -1;
 		$args['post_type']			  = $postType;
 
