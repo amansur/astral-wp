688952175672fb24d55cda1f8c43203895f2ee86
Arafat Mansur <amansur@gmail.com>
2016-09-18 12:39:53 -0400

    Changed service root reference

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 /// <reference path="AppViewModel.js" />
 var $projectNode, $homeNode, $menu, $project, $topAnchor, $projectImage, $astralBanner, $wpAdminBar, $body, $backgroundImages, $tags;
-var serviceRoot = "http://astr.nsur.org/wp-json";
+var serviceRoot = "";
 var previousRoute = '';
 var appVM = new AppViewModel();
 var backgroundImages = new Array();
